23752d2ac66dfddb12eeeded80101f77d0b87015
bunnie <bunnie@kosagi.com>
2026-03-26 02:13:37 +0800

    add bitmaps and conversion methdology
    this allows us to more quickly make "pretty" UIs by
    faking a bunch of stuff using full-screen bitmaps

---
+++ src/bitmaps/mod.rs
@@ -0,0 +1,9 @@
+pub mod dc_logo;
+pub mod tour_welcome;
+
+pub mod factory_fail;
+pub mod factory_jogpress;
+pub mod factory_leftright;
+pub mod factory_middlescan;
+pub mod factory_pass;
+pub mod factory_updown;
